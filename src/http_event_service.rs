f61f94b0f57ad571a5bd5c3ba5759282b60066cc
st170001 <st170001@stud.uni-stuttgart.de>
2024-03-20 23:09:40 +0100

    Product variant event insertion bugfix v2

--- src/http_event_service.rs
+++ src/http_event_service.rs
@@ -350,7 +350,7 @@ async fn update_product_variant_in_mongodb(
     let product_variant_version = ProductVariantVersion::from(product_variant_version_event_data);
     match collection
         .update_one(
-            doc! {"_id": product_variant._id },
+            doc! {"_id": product_variant._id},
             doc! {"$set": {"current_version": product_variant_version}},
             None,
         )
